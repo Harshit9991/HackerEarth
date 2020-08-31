4c12ef7cadd84690c997dfe14750b9d8d37af702
Harshit9991 <62600901+Harshit9991@users.noreply.github.com>
2020-08-31 09:05:21 +0530

    Create StackOperations.py

---
+++ Data Structures/Stacks/Basics of Stacks/StackOperations.py
@@ -0,0 +1,13 @@
+n,k = map(int,input().split())
+stack = [int(x) for x in imput().split()]
+
+if k == n or (n == 1 and k % 2 == 1):
+    print(-1)
+elif k > n:
+    print(max(arr))
+else:
+    t = -1
+    for i in range(0,k-1):
+        t = max(t,arr[i])
+    ans = max(t,arr[k])
+    print(ans)
